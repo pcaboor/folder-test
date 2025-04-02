eb2b84f215b957b0b04cff222e1f7d3e671b562f
Pierre Caboor <134271265+pcaboor@users.noreply.github.com>
2025-04-02 16:46:18 +0200

    Update largeFile.tsx

--- test/largeFile.tsx
+++ test/largeFile.tsx
@@ -466,6 +466,7 @@ const getFileCount = async (path: string, octokit: Octokit, githubOwner: string,
         '.gitignore'
     ];
 
+    dddddd
     const allIgnoredFiles = [...defaultIgnoredFiles, ...ignoresFilesExtensions];
 
 
